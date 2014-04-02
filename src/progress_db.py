ffbb45455becd58871ca0fb4910926fb86ffcb9c
Bryce Jasmer <jasmerb@yahoo.com>
2014-04-01 19:29:10 -0700

    Two intertwined changes not worth separating out.
    1. Move a big block outside of a try/except/else clause and just run the else part if the exception didn't return.
    
    2. Added progress saving so that we don't process tracks that have been processed a dozen times before.

--- src/progress_db.py
+++ src/progress_db.py
@@ -10,14 +10,33 @@ class ProgressDB(object):
     def __init__(self, config):
         self.logger = logging.getLogger()
 
-        db_path = os.path.join(config.storage.root_path, config.storage.db)
-        db_path = os.path.expanduser(db_path)
+        self.db_path = os.path.join(config.storage.root_path, config.storage.db)
+        self.db_path = os.path.expanduser(self.db_path)
         try:
-            db_file = open(db_path, 'r')
+            db_file = open(self.db_path, 'rb')
         except IOError as exception:
-            self.logger.debug('DB file at {0} isn\'t readable: {1}. Using empty set of processed files.'.format(db_path, exception))
-            self.progress_list = []
+            self.logger.debug('DB file at {0} isn\'t readable: {1}. Using empty set of processed files.'.format(self.db_path, exception))
+            self.progress_dict = {}
         else:
             unpickler = pickle.Unpickler(db_file)
-            self.progress_list = unpickler.load()
+            self.progress_dict = unpickler.load()
             db_file.close()
+
+    def _write(self):
+        self.logger.debug('Writing out new pickled version of progress database.')
+        with open(self.db_path, 'wb') as fd:
+            pickle.dump(self.progress_dict, fd)
+
+    def add(self, entry):
+        self.logger.debug('Appending ({0}) to progress_list.'.format(entry))
+        self.progress_dict[os.path.basename(entry)] = entry
+        self._write()
+
+    def exists(self, entry):
+        if os.path.basename(entry) in self.progress_dict:
+            return True
+        return False
+
+    def dump(self):
+        self.logger.debug('Dumping database entries: ({0})'.format(self.progress_dict))
+
