bce3d0cc0562d4157374af9228def06b77213b0a
Bryce Jasmer <jasmerb@yahoo.com>
2015-01-04 13:19:53 -0800

    when matching the inbound track, only look at the second half of the track

--- src/splices.py
+++ src/splices.py
@@ -10,14 +10,22 @@ import gpxpy.gpx
 class Splices(object):
 
     def __init__(self, config):
+        """
+        :param config:
+        :type config:
+        :return:
+        :rtype:
+        """
         self.logger = logging.getLogger()
         self.config = config
         self.splices = self.config.splices
         self.precision = self.splices.precision
         self.outbounds = []
         self.inbounds = []
+
         for outbound in self.splices.outbounds:
             d = dict()
+            d['name'] = outbound['name']
             d['location'] = gpxpy.geo.Location(outbound['location'][0], outbound['location'][1])
             d['splice_path'] = outbound['splice_path']
             outbound_path = os.path.expanduser(os.path.join(self.splices['root_path'], outbound['splice_path']))
@@ -28,6 +36,7 @@ class Splices(object):
         self.logger.debug('Outbound Splices: {0}'.format(self.outbounds))
         for inbound in self.splices.inbounds:
             d = dict()
+            d['name'] = inbound['name']
             d['location'] = gpxpy.geo.Location(inbound['location'][0], inbound['location'][1])
             d['splice_path'] = inbound['splice_path']
             inbound_path = os.path.expanduser(os.path.join(self.splices['root_path'], inbound['splice_path']))
@@ -45,12 +54,17 @@ class Splices(object):
         Return the first splice that is within the precision desired of the
         first track and first segment.
         """
-        self.logger.debug('outbound_splice_for_gpx named: {0}'.format(g.name))
+        self.logger.debug('searching for outbound splice for gpx dated: {0}'.format(g.tracks[0].segments[0].points[0].time))
         for outbound in self.outbounds:
             (location, track_no, track_segment_no, track_point_no) = g.get_nearest_location(outbound['location'])
             dist = location.distance_2d(outbound['location'])
             if dist < self.precision:
-                self.logger.debug('found close splice: outbound[\'location\']={0}, (l,t,s,n) = {1}'.format(outbound['location'], (str(location), track_no, track_segment_no, track_point_no)))
+                self.logger.debug('found close splice: name="{name}", outbound[\'location\']={location}, dist={dist}, (l,t,s,n) = {quad}'.format(
+                    name=outbound['name'],
+                    location=outbound['location'],
+                    dist=dist,
+                    quad=(str(location), track_no, track_segment_no, track_point_no)
+                ))
                 results = (outbound['splice_segment'], track_no, track_segment_no, track_point_no)
                 return results
             else:
@@ -65,13 +79,21 @@ class Splices(object):
         Return the first splice that is within the precision desired of the
         first track and first segment.
         """
-        self.logger.debug('inbound_splice_for_gpx: {0}'.format(g.name))
+        self.logger.debug('searching for inbound splice for gpx dated: {0}'.format(g.tracks[0].segments[0].points[0].time))
+        # TODO: only compare second half of segment, but return index based on entire segment
+        halfway_index = len(g.tracks[0].segments[0].points) // 2
+        second_half = gpxpy.gpx.GPXTrackSegment(g.tracks[0].segments[0].points[halfway_index:])
         for inbound in self.inbounds:
-            (location, track_no, track_segment_no, track_point_no) = g.get_nearest_location(inbound['location'])
+            (location, track_point_no) = second_half.get_nearest_location(inbound['location'])
             dist = location.distance_2d(inbound['location'])
             if dist < self.precision:
-                self.logger.debug('found close splice: inbound[\'location\']={0}, (l,t,s,n) = {1}'.format(inbound['location'], (str(location), track_no, track_segment_no, track_point_no)))
-                results = (inbound['splice_segment'], track_no, track_segment_no, track_point_no)
+                self.logger.debug('found close splice: name="{name}", inbound[\'location\']={location}, dist={dist}, (l,n) = {tuple}'.format(
+                    name=inbound['name'],
+                    location=inbound['location'],
+                    dist=dist,
+                    tuple=(str(location), track_point_no)
+                ))
+                results = (inbound['splice_segment'], 0, 0, halfway_index + track_point_no)
                 return results
             else:
                 self.logger.debug('too far: {0} meters - {1} and {2}'.format(dist, location, inbound['location']))
@@ -80,3 +102,4 @@ class Splices(object):
     #@property
     #def outbounds(self):
     #    return self.splices.outbounds
+
