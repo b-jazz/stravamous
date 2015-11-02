940d97a79188d9d6affa594e631db12e9876ada0
Bryce Jasmer <jasmerb@yahoo.com>
2015-11-01 19:59:07 -0800

    Only search the first half of the ride for outbound matches, avoiding inbound points that might be closer than outbound.

--- src/splices.py
+++ src/splices.py
@@ -55,20 +55,25 @@ class Splices(object):
         first track and first segment.
         """
         self.logger.debug('searching for outbound splice for gpx dated: {0}'.format(g.tracks[0].segments[0].points[0].time))
+        self.logger.debug('TrksSegsPts: {0}/{1}/{2}'.format(len(g.tracks),
+                                                            len(g.tracks[0].segments),
+                                                            len(g.tracks[0].segments[0].points)))
+        halfway_index = len(g.tracks[0].segments[0].points) // 2
+        first_half = gpxpy.gpx.GPXTrackSegment(g.tracks[0].segments[0].points[0:halfway_index])
         for outbound in self.outbounds:
-            (location, track_no, track_segment_no, track_point_no) = g.get_nearest_location(outbound['location'])
+            (location, track_point_no) = first_half.get_nearest_location(outbound['location'])
             dist = location.distance_2d(outbound['location'])
             if dist < self.precision:
-                self.logger.debug('found close splice: name="{name}", outbound[\'location\']={location}, dist={dist}, (l,t,s,n) = {quad}'.format(
+                self.logger.debug('found close splice: name="{name}", outbound[\'location\']={location}, dist={dist}m, (l,n) = {tuple}'.format(
                     name=outbound['name'],
                     location=outbound['location'],
                     dist=dist,
-                    quad=(str(location), track_no, track_segment_no, track_point_no)
+                    tuple=(str(location), track_point_no)
                 ))
-                results = (outbound['splice_segment'], track_no, track_segment_no, track_point_no)
+                results = (outbound['splice_segment'], 0, 0, track_point_no)
                 return results
             else:
-                self.logger.debug('too far: {0} meters - {1} and {2}'.format(dist, location, outbound['location']))
+                self.logger.debug('too far: {0}m - {1} and {2}'.format(dist, location, outbound['location']))
         return None, None, None, None
 
     def inbound_splice_for_gpx(self, g):
@@ -80,14 +85,18 @@ class Splices(object):
         first track and first segment.
         """
         self.logger.debug('searching for inbound splice for gpx dated: {0}'.format(g.tracks[0].segments[0].points[0].time))
-        # TODO: only compare second half of segment, but return index based on entire segment
+
+        self.logger.debug('TrksSegsPts: {0}/{1}/{2}'.format(len(g.tracks),
+                                                            len(g.tracks[0].segments),
+                                                            len(g.tracks[0].segments[0].points)))
         halfway_index = len(g.tracks[0].segments[0].points) // 2
         second_half = gpxpy.gpx.GPXTrackSegment(g.tracks[0].segments[0].points[halfway_index:])
+        self.logger.debug('Halfway index: {0}, point: {1}'.format(halfway_index, second_half.points[0]))
         for inbound in self.inbounds:
             (location, track_point_no) = second_half.get_nearest_location(inbound['location'])
             dist = location.distance_2d(inbound['location'])
             if dist < self.precision:
-                self.logger.debug('found close splice: name="{name}", inbound[\'location\']={location}, dist={dist}, (l,n) = {tuple}'.format(
+                self.logger.debug('found close splice: name="{name}", inbound[\'location\']={location}, dist={dist}m, (l,n) = {tuple}'.format(
                     name=inbound['name'],
                     location=inbound['location'],
                     dist=dist,
@@ -96,7 +105,7 @@ class Splices(object):
                 results = (inbound['splice_segment'], 0, 0, halfway_index + track_point_no)
                 return results
             else:
-                self.logger.debug('too far: {0} meters - {1} and {2}'.format(dist, location, inbound['location']))
+                self.logger.debug('too far: {0}m - {1} and {2}'.format(dist, location, inbound['location']))
         return None, None, None, None
 
     #@property
